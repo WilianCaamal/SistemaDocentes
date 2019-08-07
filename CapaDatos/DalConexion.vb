b97a06b72fe21b5cbdf8b22a26971209c7fc6711
Wilian Misael Caamal Caamal <wiliancaamal31@gmail.com>
2019-08-06 22:49:32 -0500

    modificando dalconexion

--- CapaDatos/DalConexion.vb
+++ CapaDatos/DalConexion.vb
@@ -1,7 +1,6 @@
 ﻿Imports FirebirdSql.Data.FirebirdClient
 
 Public Class DalConexion
-
     Public Sub GuardarConexion(StringConnection As String)
         My.Settings.cadenaConexion = StringConnection
         My.Settings.Save()
